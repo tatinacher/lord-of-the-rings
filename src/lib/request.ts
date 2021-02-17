051a25c5f80ca190c5c52dfdb94c8962faf8e337
Tatiana Cherednichenko <t.cherednichenko@redmadrobot.com>
2021-02-17 16:27:22 +0300

    add request

---
+++ src/lib/request.ts
@@ -0,0 +1,37 @@
+export type RequestProps = {
+  method: string;
+  url: string;
+  params?: object;
+};
+
+export const request = async ({
+  url,
+  method,
+  params = {}
+}: RequestProps): Promise<any> => {
+  const headers = {
+    "Content-Type": "application/json;charset=UTF-8",
+    Authorization: "Bearer NkDmdWH24AxH8TO1dXWA",
+    Accept: "application/json"
+  };
+  const options =
+    method === "get"
+      ? { headers, method }
+      : {
+          headers,
+          method,
+          body: JSON.stringify(params)
+        };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw new Error(result);
+    }
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
